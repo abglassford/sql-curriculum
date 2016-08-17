2ccff8b893887bd36364232f5c9b288239f5ce5c
Jeff Dean <jeff@galvanize.com>
2016-08-17 09:54:24 -0600

    configure and ignore index.js

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 module.exports = [
   {
-    title: '',
+    title: 'Intro to SQL',
     links: [
-      {text: '', url: ''},
+      {text: 'Intro to Relational Databases', url: './Intro.md)'},
+      {text: 'Installing PostgreSQL with Homebrew', url: './Install - Homebrew.md)'},
+      {text: 'OR Installing PostgreSQL with Postgres.app', url: './Install - App.md)'},
+      {text: 'PSQL - PostgreSQL Command Line Tools', url: './PSQL.md)'},
+      {text: 'DDL and DML', url: './SQL Intro.md)'},
+      {text: 'SQL Operators', url: './Operators.md)'},
+      {text: 'Common Functions', url: './Common Functions.md)'},
+      {text: 'Introduction to Joins and Relationships', url: './Joins.md)'},
+      {text: 'Entity Relationship Diagrams', url: './Entity Relationship Diagrams.md)'},
+      {text: 'Normalization', url: './Normalization.md)'},
+      {text: 'Indexes', url: './Indexes.md)'},
+      {text: 'Sets and Subqueries', url: './Sets and Subqueries.md)'},
+      {text: 'Roles and Permissions', url: './Roles and Permissions.md)'},
+      {text: 'Views', url: './Views.md)'},
     ]
   },
 ];
